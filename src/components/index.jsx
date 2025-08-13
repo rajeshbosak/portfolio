00ffb889393404fc6ec1eb24101d5c13f24721b6
rajeshbosak <rajeshbosak89@gmail.com>
2025-08-13 07:46:20 +0530

    Refactor: Remove commented-out exports for unused components in index file

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,9 +1,2 @@
 export { default as Navbar } from "./Navbar";
-// export { default as Hero } from "../pages/Hero";
 export { default as ParticlesBackground } from "./ParticlesBackground";
-// export { default as Skills } from "../pages/Skills";
-// export { default as CareerPath } from "../pages/CareerPath";
-// export { default as Projects } from "../pages/Projects";
-// export { default as About } from "../pages/About";
-// export { default as ResumeSection } from "../pages/ResumeSection";
-// export { default as Socials } from "../pages/Socials";
