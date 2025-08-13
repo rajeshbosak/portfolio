c53d1000ed26d135d90f117013ac7ccd807e0ce1
rajeshbosak <rajeshbosak89@gmail.com>
2025-08-13 08:38:44 +0530

    Refactor: Update router imports and clean up commented code in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter as Router } from "react-router-dom";
-// import { HashRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
+// check with this
+import { HashRouter as Router } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Router basename="/portfolio">
+    {/* <Router basename="/portfolio"> */}
+    <Router>
       <App />
     </Router>
   </StrictMode>
